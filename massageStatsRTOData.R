188314399f772ed4e0280adf4d52dee1133a2a31
Chris Auld <chris.auld@auld.nz>
2017-03-25 14:13:53 +1300

    Not quite done
    Didn't quite get InfoShare data joined to MBIE data

--- massageStatsRTOData.R
+++ massageStatsRTOData.R
@@ -3,6 +3,5 @@ dtRTO <- fread("./RTO_Occupancy_2009_t_2015.csv")
 dtRTO[,Year:=sapply(strsplit(V1,"M"),"[[",1)]
 dtRTO[,Month:=sapply(strsplit(V1,"M"),"[[",2)]
 head(dtRTO)
-dtRTO[,lapply(.SD, mean, na.rm=TRUE),by="Year",.SDcols=-c("V1","Month","Year")]
-
-
+dtRTO <- dtRTO[,lapply(.SD, mean, na.rm=TRUE),by="Year",.SDcols=-c("V1","Month","Year")]
+melt(dtRTO,id.vars = "Year")
--- mbieExample.R
+++ mbieExample.R
@@ -14,15 +14,19 @@ data(RTO)
 data(RTEs)
 data(IVStrips)
 
+
+baseLine <- 2009
+comparison <- 2015
+
 # Estimate compound annual growth rate
 growth <- RTEs %>%
   group_by(Territorial_Authority, Region) %>%
-  summarise(Growth = CAGR(sum(Spend[YEMar == 2013]) / sum(Spend[YEMar == 2009]), 4)) %>%
+  summarise(Growth = CAGR(sum(Spend[YEMar == comparison]) / sum(Spend[YEMar == baseLine]), 4)) %>%
   data.frame()
 
 # Merge with the absolute size of tourism in 2013
 RTE2 <- RTEs %>%
-  filter(YEMar == 2013) %>%
+  filter(YEMar == comparison) %>%
   group_by(Territorial_Authority, Region) %>%
   summarise(Spend = sum(Spend)) %>%
   inner_join(growth, by = c("Territorial_Authority", "Region")) %>%
@@ -35,29 +39,11 @@ treemap(RTE2,
         vSize = "Spend",
         vColor = "Growth",
         palette = "Spectral",
-        title = "Regional Tourism 2013, and CAGR 2009 to 2013",
+        title = sprintf("Regional Tourism %s, and CAGR %s to %s",comparison,baseLine,comparison),
         type = "value",
         inflate.labels = TRUE,
         algorithm = "squarified")
 
-
-
-tmp <- IVStrips %>%
-  group_by(YearQuarter, CountryGroup(COPRTop5)) %>%
-  summarise(Total = sum(SmoothTotalSpend * FinalWeight * Adjusted_Factor)) %>%
-  data.frame()
-
-tmp$period <- with(tmp, as.numeric(substring(YearQuarter, 1, 4)) + (as.numeric(substring(YearQuarter, 6, 6))-.5)/4)
-names(tmp)[2] <- "Country"
-
-ggplot(tmp, aes(x = period, y = Total / 10^6, color = Country)) +
-  stat_sa(frequency = 4, geom = "point", size = 2) +
-  stat_sa(frequency = 4, size = 2) +
-  geom_line(frequency = 4, size = 0.8, alpha = 0.4) +
-  theme_economist() +
-  labs(x = "", y = "Total spend ($m)\n") +
-  scale_color_manual("", values=tourism.cols("Alternating")) +
-  ggtitle("Seasonally adjusted NZ tourism spend by country of origin")
 
 
 # Aggregate domestic tourism spend and average annual growth rate
@@ -65,8 +51,8 @@ dom <- RTEs %>%
   filter(Type == "Domestic") %>%
   group_by(RTO) %>%
   summarise(
-    Growth = CAGR(sum(Spend[YEMar==2013]) / sum(Spend[YEMar==2009]), 4),
-    Spend2013 = sum(Spend[YEMar == 2013])
+    Growth = CAGR(sum(Spend[YEMar==comparison]) / sum(Spend[YEMar==baseLine]), 4),
+    SpendComparison = sum(Spend[YEMar == comparison])
   ) %>%
   data.frame()
 
@@ -75,11 +61,16 @@ library(data.table)
 dtRTO <- fread("./RTO_Occupancy_2009_t_2015.csv")
 dtRTO[,Year:=sapply(strsplit(V1,"M"),"[[",1)]
 dtRTO[,Month:=sapply(strsplit(V1,"M"),"[[",2)]
-head(dtRTO)
-dtRTO[,lapply(.SD, mean, na.rm=TRUE),by="Year",.SDcols=-c("V1","Month","Year")]
+dtRTO <- dtRTO[,lapply(.SD, mean, na.rm=TRUE),by="Year",.SDcols=-c("V1","Month","Year")]
+dtRTO <- melt(dtRTO,id.vars = "Year")
+setnames(dtRTO,c("Year","variable","value"),c("Year","RTO","Value"))
 
 # merge the RTE with the data from the data slot of the RTO map
 data2 <- merge(RTO@data, dom, by="RTO")
+
+#AHHHHHHHHHHH Using stringdist because the names are different
+dtRTO2 <- as.data.table(RTO)
+
 
 # replace the data slot of our RTO map with the new data frame
 RTO@data <- data2
@@ -98,12 +89,12 @@ cols <- c(colorRampPalette(c("red", "grey95"))(lessthanzero), colorRampPalette(c
 coords <- SpatialPoints(coordinates(RTO))
 
 # how big will the circles be?  "sizes" variable for later use
-sizes <- sqrt(RTO@data$Spend2013)/8
+sizes <- sqrt(RTO@data$SpendComparison)/8
 
 # set parameters so no axis line ie no box around the plot
 trellis.par.set("axis.line", list(col=NA,lty=1,lwd=1))
 
 spplot(RTO, zcol="Growth", col.regions=cols, main="Domestic tourist spend", col="white", 
        sp.layout=list("sp.points", coords, pch=1, col="black", lwd=2, cex=sizes)) # add circles layer
-grid.text("Circle size is proportional to\ndomestic tourism spend in 2013", .2,.7)
-grid.text("Average growth per year in domestic tourism 2009 - 2013", .93,.5, , rot=-90)
+grid.text(paste("Circle size is proportional to\ndomestic tourism spend in",comparison), .2,.7)
+grid.text(paste("Average growth per year in domestic tourism",sep = "-", baseLine,comparison), .93,.5, , rot=-90)
